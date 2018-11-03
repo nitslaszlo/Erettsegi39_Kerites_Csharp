a49f4d7f6f4bc33661733c44573cc0eb899fea53
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2018-11-03 22:04:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Erettsegi39_Kerites_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
